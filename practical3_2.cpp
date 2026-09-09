6f7e16821561346006e8bed81a3e32e704279aaf
kunj275patel <kunj275patel@gmail.com>
2026-09-09 13:32:19 +0530

    Update practical3_2.cpp

--- practical3_2.cpp
+++ practical3_2.cpp
@@ -8,67 +8,62 @@ int main()
 {
    
     
-
-    //subjects marks
+ //subjects marks
     float maths=80;
+    
     float cpf=70;
-    float total;
    
+    float total;
+    float average;
+    float percentage;
     float result;
 
     //Input Student Details
     
-    //cout <<" Enter Your Mathematics Marks :";
-    //cin >> maths; 
 
+   
+    
+    cout <<" Enter Your Mathematics Marks :";
+    cin >> maths; 
 
-    //cout <<" Enter Your Cpf Marks :";
-    //cin >> cpf; 
+   
+
+    cout <<" Enter Your Cpf Marks :";
+    cin >> cpf; 
 
     total=maths+cpf;
     average=(maths+cpf)/3;
     percentage=(total/200) * 100;
 
     ++cpf;
-     cout <<left<< setw(30)<<"CPF Marks"
+     cout <<left<< setw(30)<<"CPF Marks after ++cpf;"
     << cpf <<endl;
     
     cpf++; 
-    cout <<left<< setw(30)<<"CPF Marks"
+    cout <<left<< setw(30)<<"CPF Marks after cpf++"
     << cpf <<endl;
     
     --cpf; 
-    cout <<left<< setw(30)<<"CPF Marks"
+    cout <<left<< setw(30)<<"CPF Marks after --cpf"
     << cpf <<endl;
 
     cpf--; 
-    cout <<left<< setw(30)<<"CPF Marks"
+    cout <<left<< setw(30)<<"CPF Marks after cpf--"
     << cpf<<endl;
     
     
 
     //display repport
-
-
-    cout <<left<< setw(30)<<"Maths Marks"
-    << maths <<endl;
-    
-    cout <<left<< setw(30)<<"CPF Marks"
-    << cpf <<endl;
     
     result= ++cpf + cpf++ + --maths + ++maths - maths--;
-    cout <<left<< setw(30)<<"Result"
+    cout <<left<< setw(30)<<"Result : ++cpf + cpf++ + --maths + ++maths - maths-- ="
     << result<<endl;
 
-    cout <<left<< setw(30)<<"Maths Marks"
-    << maths<<endl;
-     cout <<left<< setw(30)<<"Cpf Marks"
-    << cpf<<endl;
-
-    result= ++cpf + eng++ + --maths + ++wtf - maths-- +phys-- - --phys;
-    cout <<left<< setw(30)<<"Result"
-    << result<<endl;
-cout<<"                                       -developed by Kunj M.Patel-26cs081";
-
+   
+     cout <<left<< setw(30)<<"Maths Marks after Evaluation"
+    << maths <<endl;
     
+    cout <<left<< setw(30)<<"CPF Marks after Evaluation"
+    << cpf <<endl;
+   
 }
